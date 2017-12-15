b95c4f4a8d707a80ab56c2f5611af5a6823ed427
Użytkownik Gość <Guest@imac-14.local>
2017-12-15 10:56:51 +0100

    init

---
+++ LAB_01_iOS/BridgingHeader.h
@@ -0,0 +1,16 @@
+//
+//  BridgingHeader.h
+//  LAB_01_iOS
+//
+//  Created by Użytkownik Gość on 15.12.2017.
+//  Copyright © 2017 wizner. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#endif /* BridgingHeader_h */
+
+#import "sqlite.h"  
+
+
--- LAB_01_iOS/ViewController.swift
+++ LAB_01_iOS/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfRecordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let dbFilePath = NSURL(fileURLWithPath: docDir).appendingPathComponent("demo.db")?.path
+        
+        var db: OpaquePointer? = nil
+        
+        if sqlite3_open(dbFilePath, &db) == SQLITE_OK {
+            print("ok")
+        } else {
+            print("fail")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func generateButton(_ sender: Any) {
+        let numberOfRecords = Int(numberOfRecordText.text!)
+        print(numberOfRecords);
+        
     }
 
 
