8d9430370f6e6228820e271c0ee84559e5da81c9
Użytkownik Gość <Guest@imac-14.local>
2017-12-15 09:59:13 +0100

    Initial Commit

---
+++ LAB_01_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LAB_01_iOS
+//
+//  Created by Użytkownik Gość on 15.12.2017.
+//  Copyright © 2017 wizner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
